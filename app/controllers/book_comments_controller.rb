fcbc63e62812d5e3b76983d42155d4e105954b40
EC2 Default User <ec2-user@ip-172-31-77-52.ec2.internal>
2021-05-02 07:51:43 +0000

    Update

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,12 +3,8 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
-    else
-      
-      render "books/show"
-    end
+    comment.save
+    redirect_to book_path(book)
   end
   
   def destroy
